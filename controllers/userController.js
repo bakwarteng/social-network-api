3f0cf225ff9ddbb36bb802ed40b519092393dd4b
bakwarteng <bakwarteng@hotmail.com>
2024-05-27 11:02:23 -0500

    updated error messages, updated readme, and included video link

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,17 +54,19 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  // Delete a user and associated apps
+  // Delete a user and associated thoughts
   async deleteUser(req, res) {
     try {
       const user = await User.findOneAndDelete({ _id: req.params.userId });
 
       if (!user) {
-        return res.status(404).json({ message: "No user with that ID" });
+        return res
+          .status(200)
+          .json({ message: "User and associated thoughts deleted!" });
       }
 
-      await Thoughts.deleteMany({ _id: { $in: user.thoughts } });
-      res.json({ message: "User and associated thoughts deleted!" });
+      await Thoughts.findOneAndDelete({ _id: { $in: user.thoughts } });
+      res.json({ message: "User/thoughts cannot be deleted"});
     } catch (err) {
       res.status(500).json(err);
     }
